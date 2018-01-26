02874606951cfd78747b342ff81e6b24ecd4e20a
Talloir Vincent <vincenttall@hotmail.fr>
2018-01-26 15:15:07 +0100

    chat
    add error
    remove all warning

--- chat.cpp
+++ chat.cpp
@@ -1,3 +1,4 @@
+//g++ -Wall -g  -c chat.cpp -o chat.o
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -7,15 +8,16 @@
 #include <unistd.h>
 int main(int argc, char const *argv[])
 {
-    struct sockaddr_in address;
-    int sock = 0, valread;
+    unsigned  int sock = 0;
+    signed int valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
     char buffer[1024] = {0};
     std::string ip="";
-    int port = 8001;
+    std::string msg="Hello from client";
     std::string pseudo="";
-    int minSizepseudo=5;
+    unsigned int minSizepseudo=5;
+    unsigned int port = 8001;
+
     bool checkServ = false;
     bool checkSock = false;
     int errorCount=0;
@@ -34,19 +36,18 @@ int main(int argc, char const *argv[])
 	}
 	errorCount=0;
   	while(!checkServ){
-		printf("Entrer l'ip du serveur:\n");
+		printf("entrer votre ip:");
 		std::cin >> ip;
 		memset(&serv_addr, '0', sizeof(serv_addr));
 
 		serv_addr.sin_family = AF_INET;
 		serv_addr.sin_port = htons(port);
-		// Convert IPv4 and IPv6 addresses from text to binary form
+		//connection server
 		if(inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr)<=0)
 		{
-			printf("Invalid address(%s:%d)/ Address not supported \n",ip,port);
+			printf("Invalid address(%s:%d)/ Address not supported \n",ip.c_str() ,port);
 			errorCount++;
 		}else{
-			//connection server
 			if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 			{
 				printf("Connection Failed \n");
@@ -67,16 +68,19 @@ int main(int argc, char const *argv[])
 			printf("Votre pseudo est trop cour (%d caractère minimum)\n",minSizepseudo);
   		}
   	}
-
+//si startport!= port
 //|attendre que le pseudo soit ajouté	attendre une requette type 1&pseudo
 //|affichage serv /info/input			faire une requette 0 pour la liste et 1 pour les info du serv reponse type 2&... sauvegarde des room dans un tableau
-//|entrer room							
+//finsi						
+//|entrer room	
 // |ecrire message(si press enter arreter affichage et faire un cin)
 // |changer room(press echap)
-
-    send(sock , hello , strlen(hello) , 0 );
+    send(sock , msg.c_str() , msg.length() , 0 );
     printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    valread = read( sock , buffer,1337);
+    if(valread == 1337)
+    	printf("error read\n");
+    else
+    	printf("%s\n",buffer );
     return 0;
 }
