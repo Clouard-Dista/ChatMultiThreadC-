3701d3578b462274bcd262e4bf714a43a4a19331
Talloir Vincent <vincenttall@hotmail.fr>
2018-01-29 19:03:55 +0100

    add server

--- server.cpp
+++ server.cpp
@@ -1,59 +1,187 @@
-//g++ -Wall -g  -c server.cpp -o server.o
+// Server side C/C++ program to demonstrate Socket programming
+#include <sstream>      //stringstream
+#include <map>          //element map
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <string>
 #include <unistd.h>
-#define PORT 8001
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+#include <thread>
+#include <iostream>
+#include <vector>
+
+int maxRoom=5;
+std::map <int, int> roomList;
+
+std::string readToString(unsigned  int sock){
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    signed int valRead;
+    std::string tot;
+    while(true){
+        valRead = read( sock , buffer,1024);
+        if(valRead!=1024){
+            buffer[valRead]='\0';
+        }
+        std::stringstream ss;
+        ss << tot << buffer;
+        tot = ss.str();
+        if(valRead!=1024){
+            return tot;
+        }
     }
+}
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+#define PORT 8001
+void home(){
+    std::map <std::string, std::string> pseudoList;
+    std::vector <std::thread> roomVal;
+    std::string text="";
+    int opt = 1;
+    char buffer[1024] = {0};
+    std::string comPseu="70";
+    std::string comInfo="71";
+    std::string comMess="50";
+    std::string msghello = "Hello from server";
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-    {
+    int server_fd, new_socket, valread;
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 
+    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0){
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
-    {
+    if (listen(server_fd, 3) < 0){
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
-    {
+    printf("en attente de conection\n");
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    printf("nouvelle conection\n");
+    while(true){
+        printf("en attente de message\n");
+        std::string msgRead = readToString(new_socket);
+        printf("|%s|\n",msgRead.c_str());
+
+        if(msgRead.compare(0,2,comPseu)==0){
+            printf("pseudo 1\n");
+            std::string msgReturn ="1";
+            printf("pseudo 2\n");
+            msgRead = msgRead.substr(2,msgRead.length()-1);
+            printf("pseudo 3\n");
+            pseudoList[msgRead]=msgRead;
+
+
+            send(new_socket , msgReturn.c_str() , msgReturn.length() , 0 );
+            printf("pseudo %s validé\n",(msgRead.c_str()) );
+        }
+        if(msgRead.compare(0,2,comInfo)==0){
+            printf("info 1\n");
+            std::string msgInfo ="0";
+            for (int i = 0; i < maxRoom; ++i){
+                std::stringstream ss;
+                ss << roomList[i];
+                if(i+1 < maxRoom)
+                    ss << "&";
+                msgInfo = ss.str();
+            }
+            printf("info 2\n");
+            send(new_socket , msgInfo.c_str() , msgInfo.length() , 0 );
+            printf("info 3\n");
+        }
+    }
+}
+
+void addroom(int portRoom){
+    std::map <std::string, std::string> pseudoList;
+    std::string text="";
+    int opt = 1;
+    char buffer[1024] = {0};
+    std::string comPseu="70";
+    std::string comInfo="71";
+    std::string comMess="50";
+    std::string msghello = "Hello from server";
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( portRoom );
+
+    int server_fd, new_socket, valread;
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 
+    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0){
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0){
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    printf("r%d|en attente de conection\n",portRoom);
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    printf("r%d|nouvelle conection\n",portRoom);
+    while(true){
+        printf("r%d|en attente de message\n",portRoom);
+        std::string msgRead = readToString(new_socket);
+        printf("|%s|\n",msgRead.c_str());
+    }
+}
+
+int main(int argc, char const *argv[]){
+    for (int i = 0; i < maxRoom; ++i){
+        roomList[i]=8002+i;
+    }
+    std::thread th([]() {
+        home();
+    });
+    std::thread t1([]() {
+        addroom(8002);
+    });
+    std::thread t2([]() {
+        addroom(8003);
+    });
+    std::thread t3([]() {
+        addroom(8004);
+    });
+    th.join();
+    t1.join();
+    t2.join();
+    t3.join();
     return 0;
 }
+
